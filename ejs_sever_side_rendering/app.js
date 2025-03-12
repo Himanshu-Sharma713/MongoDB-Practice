a00a86cbac030cdd14de3fe43e7982fbc21a172a
Himanshu-Sharma713 <himanshusb713@gmail.com>
2025-03-12 12:05:09 +0530

    setup all folder & files

--- ejs_sever_side_rendering/app.js
+++ ejs_sever_side_rendering/app.js
@@ -1,10 +1,16 @@
 import express from "express";
+import path from "node:path";
 
 const app = express();
 const PORT = 3001;
 
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(import.meta.dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("<h1> CRUD Operations with EJS & Server-Side Rendering </h1>");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
